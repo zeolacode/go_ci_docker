20f65a6abdf8872a276d5e973d21c13139d22654
pedro.zeola <pedro.zeola@ubivis.io>
2023-09-28 10:36:32 -0300

    update go.yml

--- database/db.go
+++ database/db.go
@@ -15,7 +15,7 @@ var (
 
 func ConectaComBancoDeDados() {
 
-    stringDeConexao := "host="+os.Getenv("HOST")+" user=root password=root dbname=root port=5432 sslmode=disable"
+    stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="os.Getenv("DBNAME")+" port="os.Getenv("PORT")+" sslmode=disable"
     DB, err = gorm.Open(postgres.Open(stringDeConexao))
     if err != nil {
         log.Panic("Erro ao conectar com banco de dados")
